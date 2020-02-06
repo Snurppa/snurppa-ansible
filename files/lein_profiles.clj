bff8adb209e17eba60977c8a76f73769f2ad233b
Joni Hämäläinen <joni.hamalainen@evolta.fi>
2020-02-06 14:16:11 +0200

    lein profiles

--- files/lein_profiles.clj
+++ files/lein_profiles.clj
@@ -3,15 +3,16 @@
                   [lein-kibit "0.1.6"]
                   [lein-collisions "0.1.4"]
                   [lein-ancient "0.6.15"]]
-;        :test-paths ["test", "itest"]
+
         :dependencies [[slamhound "1.5.5"]
                        [im.chit/vinyasa "0.4.7"]
-                       [org.clojars.gjahad/debug-repl "0.3.3"]]
+                       [org.clojars.gjahad/debug-repl "0.3.3"]
+                       [hashp "0.1.1"]]
         :injections [(require '[vinyasa.inject :as inject])
                      (require 'alex-and-georges.debug-repl)
+                     (require 'hashp.core)
                      (inject/in
                        clojure.core >
                        [clojure.pprint pprint]
                        [alex-and-georges.debug-repl debug-repl])]}}
 
-(defn spy [x] (println x) x)
