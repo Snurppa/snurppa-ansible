b230e161d75338d4ad5e04773c1d68a1efb7fa5f
Joni Hämäläinen <joni.hamalainen@quuppa.com>
2022-06-01 10:17:16 +0300

    bump profiles.clj

--- files/lein_profiles.clj
+++ files/lein_profiles.clj
@@ -1,18 +1,15 @@
-{:user {:plugins [[lein-pprint "1.2.0"]
-                  [jonase/eastwood "0.2.4"]
-                  [lein-kibit "0.1.6"]
+{:user {:plugins [[lein-pprint "1.3.2"]
+                  [jonase/eastwood "1.2.3"]
+                  [lein-kibit "0.1.8"]
                   [lein-collisions "0.1.4"]
-                  [lein-ancient "0.6.15"]]
+                  [lein-ancient "1.0.0-RC3"]]
 
-        :dependencies [[slamhound "1.5.5"]
-                       [im.chit/vinyasa "0.4.7"]
-                       [org.clojars.gjahad/debug-repl "0.3.3"]
-                       [hashp "0.1.1"]]
+        :dependencies [[im.chit/vinyasa "0.4.7"]
+                       [hashp "0.2.1" :exclusions [zprint]]
+                       [zprint "1.2.3"]]
         :injections [(require '[vinyasa.inject :as inject])
-                     (require 'alex-and-georges.debug-repl)
                      (require 'hashp.core)
                      (inject/in
                        clojure.core >
-                       [clojure.pprint pprint]
-                       [alex-and-georges.debug-repl debug-repl])]}}
+                       [clojure.pprint pprint])]}}
 
